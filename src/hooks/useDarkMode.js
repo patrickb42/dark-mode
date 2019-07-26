de1187642b6eb877e402bc97d11f0d6520021184
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-07-26 09:06:50 -0400

    made useDarkMode

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react'
+
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = ({ initialValue = true }) => {
+  const [isDark, setIsDark] = useLocalStorage({ key: 'darkMode', initialValue: initialValue });
+
+  useEffect(() => {
+    isDark
+     ? document.querySelector('body').classList.add('dark-mode')
+     : document.querySelector('body').classList.remove('dark-mode');
+  },        [isDark]);
+
+  return [isDark, setIsDark];
+};
